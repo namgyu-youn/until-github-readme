f7ab6bc50e3090521f28f6b188ac7daa18fcd885
namgyu-youn <yynk2012@gmail.com>
2025-04-20 05:07:45 +0000

    remove distribtuion files after code-review (#2) - add ignore option for distribution files - remove files in dist/ path

--- dist/utils/encodeImageBase64.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.encodeImageBase64 = encodeImageBase64;
-async function encodeImageBase64(imageUrl) {
-    return fetch(imageUrl)
-        .then((res) => res.blob())
-        .then((blob) => blob.arrayBuffer())
-        .then((buffer) => Buffer.from(buffer).toString("base64"));
-}
